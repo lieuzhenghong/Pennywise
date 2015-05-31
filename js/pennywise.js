6c05c3785ef055f896f7da08c8a31440e359a047
Lieu Zheng Hong <lieuzhenghong@gmail.com>
2015-05-31 13:15:32 +0800

    Add feature to add to account

--- js/pennywise.js
+++ js/pennywise.js
@@ -1,4 +1,6 @@
-    var ACCOUNTS = [
+'use strict';
+
+var ACCOUNTS = [
     {name: 'cash on hand'},
     {name: 'savings account'},
     {name: 'shared account'},
@@ -10,6 +12,38 @@ var TRANSACTIONS = [
     {date: '', name: 'Apples', amount: 2.50, category: 'Groceries', account: 'cash on hand'},
     {date: '', name: 'Cash withdrawal', amount: 250, account: 'savings account'}
 ];
+
+var Container = React.createClass({
+
+    getInitialState: function (){
+        return {
+            activeAccount: ACCOUNTS[0].name,
+            transactions: TRANSACTIONS
+        };
+    },
+
+    setActiveAccount: function(dat) {
+        this.setState({
+            activeAccount: dat  
+        });
+    },
+
+    render: function () {
+        return (
+            <div id = 'wrapper'>
+            <NavBar 
+                accounts = {ACCOUNTS}
+                activeAccount = {this.state.activeAccount}
+                onChange = {this.setActiveAccount}
+            />
+            <TransactionsTable 
+                transactions = {this.state.transactions}
+                activeAccount = {this.state.activeAccount}
+            />
+            </div>
+        );
+    }
+});
 
 var AccountRow = React.createClass({
 
@@ -58,7 +92,72 @@ var NavBar = React.createClass({
             </div>
         )
     }
-});
+});     
+
+var TransactionsTable = React.createClass({
+    handleTransactionSubmit: function(transaction) {
+        var data = transaction;
+        data.account = this.props.activeAccount;
+        console.log(data);
+        
+        this.setState({
+            TRANSACTIONS: TRANSACTIONS.push(data)
+        })
+    },
+    
+    render: function() {
+        return (
+            <div id = 'recent-transactions'>
+            <h2>Recent Transactions for {this.props.activeAccount}</h2>
+            <table>
+                <tr>
+                    <th>date</th>
+                    <th>name</th>
+                    <th>amount</th>
+                    <th>account</th>
+                    <th>edit </th>
+                </tr>
+                <TransactionList 
+                    transactions = {this.props.transactions}
+                    activeAccount = {this.props.activeAccount}
+                />
+
+            </table>
+            <TransactionForm onTransactionSubmit = {this.handleTransactionSubmit} />
+            </div>
+        )
+    }
+});        
+        
+var TransactionList = React.createClass ({                      
+    render: function () {
+
+        var activeaccount = this.props.activeAccount;
+
+        var rows = [];
+        this.props.transactions.map(function(each_transaction) {
+            if (each_transaction.account == activeaccount) {
+
+                /* Very strange behaviour
+                if (each_transaction account == this.props.activeAccount) 
+                DOES NOT WORK, I do not know why this is the case
+                
+                it is due to javascript scoping of the 'this' variable
+                */
+
+                rows.push(<TransactionRow transaction = {each_transaction} key = {each_transaction.name} />);
+            }
+            else {
+                /*console.log(each_transaction.account);*/
+            }     
+        })
+        return (
+            <tbody>
+            {rows}
+            </tbody>
+        )
+    }
+});        
 
 var TransactionRow = React.createClass({
     render: function (){
@@ -76,115 +175,37 @@ var TransactionRow = React.createClass({
     }
 });
 
-var TransactionList = React.createClass ({                      
-    render: function () {
 
-        var activeaccount = this.props.activeAccount;
-
-        var rows = [];
-        this.props.transactions.map(function(each_transaction) {
-            if (each_transaction.account == activeaccount) {
-
-                /* Very strange behaviour
-                if (each_transaction account == this.props.activeAccount) 
-                DOES NOT WORK, I do not know why this is the case
-                */
-
-                rows.push(<TransactionRow transaction = {each_transaction} key = {each_transaction.name} />);
-            }
-            else {
-                /*console.log(each_transaction.account);*/
-            }     
-        })
-        return (
-            <tbody>
-            {rows}
-            </tbody>
-        )
-    }
-});
-
-var TransactionsTable = React.createClass({
-    render: function() {
-
-        return (
-            <div id = 'recent-transactions'>
-            <h2>Recent Transactions for {this.props.activeAccount}</h2>
-            <table>
-                <tr>
-                    <th>date</th>
-                    <th>name</th>
-                    <th>amount</th>
-                    <th>account</th>
-                    <th>edit </th>
-                </tr>
-                <TransactionList 
-                    transactions = {this.props.transactions}
-                    activeAccount = {this.props.activeAccount}
-            />
-            </table>
-            </div>
-        )
-    }
-});
 
 var TransactionForm = React.createClass({
+    
+    handleSubmit: function( e ) {
+        e.preventDefault();
+        var name = React.findDOMNode(this.refs.t_name).value.trim();
+        var amount = React.findDOMNode(this.refs.t_amount).value.trim();
+        var cat = React.findDOMNode(this.refs.t_cat).value.trim();
+        
+        this.props.onTransactionSubmit({name: name, amount: amount, cat: cat});
+        
+        React.findDOMNode(this.refs.t_name).value = '';
+        React.findDOMNode(this.refs.t_amount).value = '';
+        React.findDOMNode(this.refs.t_cat).value = '';
+        return;
+    },
+        
     render: function() {
         return (
-            <div>
+            <form onSubmit = {this.handleSubmit}>
             <h2>Add Transaction </h2>
             <p>Transaction name</p>
-            <input type = 'text' />
-            <p>Price</p>
-            <input type = 'number'/>
+            <input type = 'text' placeholder = 'Transaction name' ref = 't_name'/>
+            <p>Amount</p>
+            <input type = 'number' placeholder = 'Amount' ref = 't_amount' />
             <p>Category (optional) </p>
-            <input type = 'text' />
-            </div>
+            <input type = 'text' placeholder = 'Category (optional)' ref = 't_cat' />
+            <input type = 'submit' value = 'Add Transaction'/>
+            </form>
         )
-    }
-});
-
-
-var ButtonMenu = React.createClass ({
-    render: function () {
-        return (
-            <div>
-            <button>Add Transaction</button>
-            </div>
-        )
-    }
-});
-
-var Container = React.createClass({
-
-    getInitialState: function (){
-        return {
-            activeAccount: ACCOUNTS[0].name
-        };
-    },
-
-    setActiveAccount: function(dat) {
-        this.setState({
-            activeAccount: dat  
-        });
-    },
-
-    render: function () {
-        return (
-            <div id = 'wrapper'>
-            <NavBar 
-                accounts = {ACCOUNTS}
-                activeAccount = {this.state.activeAccount}
-                onChange = {this.setActiveAccount}
-            />
-            <TransactionsTable 
-                transactions = {TRANSACTIONS}
-                activeAccount = {this.state.activeAccount}
-            />
-            <TransactionForm />
-            <ButtonMenu />
-            </div>
-        );
     }
 });
 
